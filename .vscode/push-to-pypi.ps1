c5b39d91c77f80d9ec9a1542f6815771b309417b
Gordon Watts <gwatts@uw.edu>
2020-02-09 21:45:30 -0800

    Push the next version to pypi
    Also improve the scripts on windows that run this push.

--- .vscode/push-to-pypi.ps1
+++ .vscode/push-to-pypi.ps1
@@ -1,7 +1,22 @@
 if(Test-Path -Path dist ){
   Remove-Item -Force -Recurse dist
 }
+if (Test-Path -Path build) {
+  Remove-Item -Force -Recurse build
+}
+if (Test-Path -Path .\*.egg-info) {
+  Remove-Item -Force -Recurse .\*.egg-info
+}
 
 python setup_xAOD.py sdist bdist_wheel
 python setup_xAOD_backend.py sdist bdist_wheel
 twine upload dist/*
+if(Test-Path -Path dist ){
+  Remove-Item -Force -Recurse dist
+}
+if (Test-Path -Path build) {
+  Remove-Item -Force -Recurse build
+}
+if (Test-Path -Path .\*.egg-info) {
+  Remove-Item -Force -Recurse .\*.egg-info
+}
