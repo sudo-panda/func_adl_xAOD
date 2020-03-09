7e7f584483d4aeda4336d5dbb1a052bb760e4787
Gordon Watts <gwatts@uw.edu>
2020-03-09 05:02:50 -0700

    Add back in sleep part of test we forgot.

--- tests/test_use_exe_servicex.py
+++ tests/test_use_exe_servicex.py
@@ -10,7 +10,7 @@ from func_adl_xAOD import use_exe_servicex
 
 async def dummy_executor_coroutine(a: ast.AST) -> ast.AST:
     'Called to evaluate a guy - but it will take a long time'
-    # await asyncio.sleep(0.01)
+    await asyncio.sleep(0.01)
     return a
 
 # @pytest.fixture()
