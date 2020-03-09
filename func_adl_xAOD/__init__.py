14ebaa10ec3cd0836b5a0b40d33368a2ee0a7aff
Gordon Watts <gwatts@uw.edu>
2020-03-09 05:26:58 -0700

    Clean up linting

--- func_adl_xAOD/__init__.py
+++ func_adl_xAOD/__init__.py
@@ -1,3 +1,3 @@
 # Main two things from this package
 # flake8: noqa
-from .use_exe_servicex import FuncADLServerException, use_exe_servicex
+from .use_exe_servicex import FuncADLServerException, use_exe_servicex  # NOQA
