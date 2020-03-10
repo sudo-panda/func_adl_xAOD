865533d3fdc50fab7a4ac4c3cec3add0808dd797
Gordon Watts <gwatts@uw.edu>
2020-03-09 21:05:29 -0700

    Bump version number
    And improve the push to pypi script

--- .vscode/push-to-pypi.ps1
+++ .vscode/push-to-pypi.ps1
@@ -1,22 +1,9 @@
-if(Test-Path -Path dist ){
-  Remove-Item -Force -Recurse dist
-}
-if (Test-Path -Path build) {
-  Remove-Item -Force -Recurse build
-}
-if (Test-Path -Path .\*.egg-info) {
-  Remove-Item -Force -Recurse .\*.egg-info
-}
-
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./dist/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./build/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse -Confirm:$false ./*.egg-info
 python setup_xAOD.py sdist bdist_wheel
 python setup_xAOD_backend.py sdist bdist_wheel
 twine upload dist/*
-if(Test-Path -Path dist ){
-  Remove-Item -Force -Recurse dist
-}
-if (Test-Path -Path build) {
-  Remove-Item -Force -Recurse build
-}
-if (Test-Path -Path .\*.egg-info) {
-  Remove-Item -Force -Recurse .\*.egg-info
-}
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./dist/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./build/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse -Confirm:$false ./*.egg-info
