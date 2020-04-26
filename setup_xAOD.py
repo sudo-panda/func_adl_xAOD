0b61a413fc81669546f058a991e42b8e4e3f168a
Gordon Watts <gwatts@uw.edu>
2020-04-25 17:11:20 -0700

    Move to next version o f func_adl
    This version solves the lambda mis-naming problems.
    
    #42

--- setup_xAOD.py
+++ setup_xAOD.py
@@ -26,7 +26,7 @@ setup(name="func_adl_xAOD",
           "pandas~=1.0",
           "uproot~=3.7",
           "retry~=0.9",
-          "func_adl.ast==1.0.0a22",
+          "func_adl.ast==1.0.0b1",
           "qastle==0.7",
           "servicex"
       ],
