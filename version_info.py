2270dc7fe542be832fe1ea88825d046333364926
Gordon Watts <gwatts@uw.edu>
2020-06-26 23:12:01 -0700

    New version to pypi

--- version_info.py
+++ version_info.py
@@ -1,6 +1,6 @@
 # Contains version numbers for the two packages this repo will make
 
-linked_version = "1.1.0b2"
+linked_version = "1.1.0b3"
 version_func_adl_xaod = linked_version
 version_func_adl_xaod_backend = linked_version
 
