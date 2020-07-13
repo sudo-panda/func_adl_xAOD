8e811b6507b4dc515728f0f95f67dff1c553783a
Gordon Watts <gordon@gordonandpaula.net>
2020-07-13 15:46:06 -0400

    Fix flake8 error

--- func_adl_xAOD/backend/datasets/gridds.py
+++ func_adl_xAOD/backend/datasets/gridds.py
@@ -11,6 +11,7 @@ from func_adl.ast.func_adl_ast_utils import (
 from func_adl.util_ast import as_ast
 import requests
 
+
 # Resolvers:
 def resolve_file(parsed_url, url: str):
     if len(parsed_url.netloc) != 0:
