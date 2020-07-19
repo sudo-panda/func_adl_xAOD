c370263b1983cb20788695a97ea1c362b2f28402
Gordon Watts <gordon@gordonandpaula.net>
2020-07-19 08:35:57 -0400

    Fix bad test control
    Ops - turned on the long running tests!

--- tests/xAODlib/control_tests.py
+++ tests/xAODlib/control_tests.py
@@ -4,8 +4,8 @@ from pathlib import Path
 import os
 
 # This mark should be turned off if we want to run long-running tests.
-# run_long_running_tests = pytest.mark.skipif(True, reason='Long running tests, skipped except when run by hand')
-run_long_running_tests = pytest.mark.skipif(False, reason="ok to run")
+run_long_running_tests = pytest.mark.skipif(True, reason='Long running tests, skipped except when run by hand')
+# run_long_running_tests = pytest.mark.skipif(False, reason="ok to run")
 
 # The file we can use in our test. It has only 10 events...
 local_path = 'tests/xAODlib/jets_10_events.root'
