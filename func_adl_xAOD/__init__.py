5c0f9bf78ccfb837154500b4f2d8382dabf881fc
Baidyanath Kundu <kundubaidya99@gmail.com>
2021-03-23 13:01:52 +0530

    Fix conflict in method_names

--- func_adl_xAOD/__init__.py
+++ func_adl_xAOD/__init__.py
@@ -1,2 +1,2 @@
-from .atlas_xAOD_lib.atlas_xaod_executor import atlas_xaod_executor  # NOQA
-from .cms_AOD_lib.cms_aod_executor import cms_aod_executor  # NOQA
+# from .atlas_xAOD_lib.atlas_xaod_executor import atlas_xaod_executor  # NOQA
+# from .cms_AOD_lib.cms_aod_executor import cms_aod_executor  # NOQA
