151a6661e71a4cd710ce6bbc109dac887bc2baff
Baidyanath Kundu <kundubaidya99@gmail.com>
2021-04-16 01:11:46 +0530

    Uncomment top level __init__.py contents

--- func_adl_xAOD/__init__.py
+++ func_adl_xAOD/__init__.py
@@ -1,2 +1,2 @@
-# from .atlas.xaod.executor import atlas_xaod_executor  # NOQA
-# from .cms.aod.executor import cms_aod_executor  # NOQA
+from .atlas.xaod.executor import atlas_xaod_executor  # NOQA
+from .cms.aod.executor import cms_aod_executor  # NOQA
