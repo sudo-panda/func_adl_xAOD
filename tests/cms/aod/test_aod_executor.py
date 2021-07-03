4a85c8bd69b0f504ba34b50d15fd9dba09ae70a4
Baidyanath Kundu <kundubaidya99@gmail.com>
2021-07-03 22:14:43 +0530

    Add short running test for member variables

---
+++ tests/cms/aod/test_aod_executor.py
@@ -0,0 +1,22 @@
+from tests.utils.locators import find_line_numbers_with, find_line_with, find_open_blocks
+from tests.utils.general import get_lines_of_code, print_lines
+import pytest
+from tests.cms.aod.utils import cms_aod_dataset, exe_from_qastle
+
+# Tests that make sure the cms aod executor is working correctly
+
+
+class CMS_AOD_File_Type:
+    def __init__(self):
+        pass
+
+
+def test_Select_member_variable():
+    r = cms_aod_dataset() \
+        .SelectMany(lambda e: e.Muons("muons")) \
+        .Select(lambda m: m.pfIsolationR04().sumChargedHadronPt) \
+        .value()
+    lines = get_lines_of_code(r)
+    _ = find_line_with(".sumChargedHadronPt", lines)
+    assert find_line_with(".sumChargedHadronPt()", lines, throw_if_not_found=False) == -1
+    
